17eb81b2fd53144d1f21455686397f917c583414
i000o <iosman1096@gmail.com>
2025-11-26 15:30:25 +0000

    js update based on feedback

--- js/script.js
+++ js/script.js
@@ -1,16 +1,42 @@
-const button = document.getElementsByClassName('toggle'); 
+// const button = document.getElementsByClassName('toggle'); 
 
-for (let i = 0; i < button.length; i++) { 
-    button[i].addEventListener("click", function() { // arrow function not applicable here 
-        this.classList.toggle("active"); 
-        const content = this.parentElement.nextElementSibling; // gets the next sibling in line
-        if (content.style.display === 'block') { // checks if this element is currently visible 
-             content.style.display = 'none'; 
-            } else { 
-                content.style.display = 'block'; 
-            }
-    }); 
-} 
+// for (let i = 0; i < button.length; i++) { 
+//     button[i].addEventListener("click", function() { // arrow function not applicable here 
+//         this.classList.toggle("active"); 
+//         const content = this.parentElement.nextElementSibling; // gets the next sibling in line
+//         if (content.style.display === 'block') { // checks if this element is currently visible 
+//              content.style.display = 'none'; 
+//             } else { 
+//                 content.style.display = 'block'; 
+//             }
+//     }); 
+// } 
 
-console.log(this.parentElement); // should be <div class="faq">
-console.log(this.parentElement.nextElementSibling); // should be <p class="answer">
+document.addEventListener("DOMContentLoaded", () => { 
+const buttons = document.getElementsByClassName('toggle'); 
+
+for (let i = 0; i < buttons.length; i++) { 
+    buttons[i].addEventListener("click", function() { 
+        const answerElement = this.parentElement.nextElementSibling; 
+
+        // Find the plus and minus icons within the clicked buttons 
+        const minusIcon = this.querySelector('.minus-icon'); 
+        const plusIcon = this.querySelector('.plus-icon'); 
+
+        if (answerElement.style.display === 'block') { 
+            answerElement.style.display = 'none';
+            minusIcon.style.display = 'none'; 
+            plusIcon.style.display = 'inline'; 
+        } else { 
+            answerElement.style.display = 'block'; 
+            minusIcon.style.display = 'inline'; 
+            plusIcon.style.display = 'none'; 
+        }
+    });
+}
+}); 
+
+// console.log(this.parentElement); // should be <div class="faq">
+// console.log(this.parentElement.nextElementSibling); // should be <p class="answer">
+console.log(buttons.length); 
+console.log(buttons[i]); 
